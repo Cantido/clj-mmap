0edd2f2e4051e8cfc1600c961389aa56bc47902a
Bobby Richter <robert.c.richter@gmail.com>
2017-11-10 12:39:01 -0600

    Build with clojure 1.5 through 1.9

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/Cantido/clj-mmap"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :deploy-repositories [["releases" :clojars]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-beta4"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
