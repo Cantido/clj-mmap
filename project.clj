17eb984c4c485e000b2381c747776ec57a6255a3
Bobby Richter <robert.c.richter@gmail.com>
2017-11-10 11:45:49 -0600

    Update version & project description for my fork.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-mmap "1.1.2"
+(defproject clj-mmap "1.2.0-SNAPSHOT"
   :description "A wrapper over java.nio's mmap() implementation to ease use, and enable mmap'ing files larger than 2GB."
-  :url "https://github.com/thebusby/clj-mmap"
+  :url "https://github.com/Cantido/clj-mmap"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
